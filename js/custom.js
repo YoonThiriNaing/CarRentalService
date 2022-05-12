7109210854ff029324b60d2c4d59fe9dbbad210f
YoonThiriNaing <cgm.yoonthirinaing@gmail.com>
2022-05-12 15:16:48 +0630

    responsive updated

---
+++ js/custom.js
@@ -0,0 +1,12 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-list");
+hamburger.addEventListener("click",()=>{
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+
+$('.dropdown ul li').hide();
+$('.nav-a').click(function() {
+    $('.dropdown ul li').toggle(200);
+});    
+
